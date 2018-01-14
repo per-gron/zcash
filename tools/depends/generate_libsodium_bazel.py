208849b7f2c78a1ab54cf6b0fa088f411dada8ca
Per Grön <per.eckerdal@gmail.com>
2018-01-14 18:57:43 +0100

    Don't force static linking of libsodium
    It's not necessary and increases linking time for unit tests. All the
    sub-libraries were merged to avoid dependency issues that occured only
    during dynamic linking.

--- tools/depends/generate_libsodium_bazel.py
+++ tools/depends/generate_libsodium_bazel.py
@@ -16,15 +16,15 @@ libsodium_config_opts = [
 
 subprocess.call(["./configure"] + libsodium_config_opts)
 
-libraries = {
-    "sodium": ["avx512f", "avx2", "sse41", "ssse3", "sse2", "aesni"],
-    "avx512f": [],
-    "avx2": [],
-    "sse41": [],
-    "ssse3": [],
-    "sse2": [],
-    "aesni": [],
-}
+libraries = [
+    "sodium",
+    "avx512f",
+    "avx2",
+    "sse41",
+    "ssse3",
+    "sse2",
+    "aesni",
+]
 
 # TODO(per-gron): This is not very nice; it assumes that the package is
 # being imported with a given name.
@@ -43,45 +43,39 @@ cflags_str = generator_util.extract_variable_from_makefile(
     "$(DEFS) $(libaesni_la_CPPFLAGS) $(CPPFLAGS) $(AM_CFLAGS) $(CFLAGS)",
     makefile)
 cflags = shlex.split(cflags_str)
-cflags = ["'%s'" % flag for flag in cflags if flag != "-g" and not re.match(r"^-O\d$", flag) and not re.match(r"^-D_FORTIFY_SOURCE=", flag)]
+cflags = ["'%s'" % flag for flag in cflags if flag != "-g" and not re.match(r"^-O\d$", flag) and not re.match(r"^-D_FORTIFY_SOURCE=", flag) and flag != "-fPIC" and flag != "-fPIE"]
 
 version_h = "src/libsodium/include/sodium/version.h"
 
-def process_library(name, deps):
+def process_libraries():
     rule = ""
 
-    def obj_to_src(obj):
-        without_ext = "src/libsodium/" + re.sub(r"/lib" + name + r"_la-(.*)\.lo$", r"/\1", obj)
+    def obj_to_src(lib_name, obj):
+        without_ext = "src/libsodium/" + re.sub(r"/lib" + lib_name + r"_la-(.*)\.lo$", r"/\1", obj)
         for ext in [".S", ".c"]:
             candidate = "%s%s" % (without_ext, ext)
             if os.path.isfile(candidate):
                 return candidate
         raise "Could not find file for %s" % without_ext
 
-    objs = shlex.split(generator_util.extract_variable_from_makefile(
-        "$(lib%s_la_OBJECTS)" % name, makefile))
-    srcs = [obj_to_src(obj) for obj in objs] + [version_h]
+    srcs = set([version_h])
+    for lib in libraries:
+        objs = shlex.split(generator_util.extract_variable_from_makefile(
+            "$(lib%s_la_OBJECTS)" % lib, makefile))
+        for obj in objs:
+            srcs.add(obj_to_src(lib, obj))
 
     rule += "cc_library(\n"
-    if name == "sodium":
-        rule += '    visibility = ["//visibility:public"],\n'
-        rule += '    hdrs = headers,\n'
-    rule += "    name = '%s',\n" % name
+    rule += '    visibility = ["//visibility:public"],\n'
+    rule += '    hdrs = headers,\n'
+    rule += "    name = 'sodium',\n"
     rule += "    copts = cflags,\n"
-    rule += "    linkstatic = 1,\n"
     rule += "    includes = ['src/libsodium/include', 'src/libsodium/include/sodium'],\n"
-    rule += "    deps = %s,\n" % [":%s" % dep for dep in deps]
-    rule += "    srcs = %s + headers,\n" % srcs
+    rule += "    srcs = %s + headers,\n" % list(srcs)
     rule += "    textual_hdrs = assembly_files,\n"
     rule += ")\n\n"
 
     return rule
-
-def process_libraries():
-    res = ""
-    for lib in libraries:
-        res += process_library(lib, libraries[lib])
-    return res
 
 build_file = generator_util.build_header()
 build_file += "cflags = %s\n" % (cflags + extra_cflags)
